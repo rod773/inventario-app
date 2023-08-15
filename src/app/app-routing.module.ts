d2df82fa35f060b1c738dc45abbff12087b82427
rod773 <104315743+rod773@users.noreply.github.com>
2023-08-15 19:16:45 +0200

    router links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductoListaComponent } from './producto-lista/producto-lista.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'productos',
+    component: ProductoListaComponent,
+  },
+  {
+    path: '',
+    redirectTo: 'productos',
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
