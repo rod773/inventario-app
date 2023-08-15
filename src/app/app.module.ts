59adb55b388e1c937dcd335fa7a13deaf08072cd
rod773 <104315743+rod773@users.noreply.github.com>
2023-08-15 17:27:21 +0200

    producto service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductoListaComponent } from './producto-lista/producto-lista.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ProductoListaComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, ProductoListaComponent],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/producto-lista/producto-lista.component.ts
+++ src/app/producto-lista/producto-lista.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { Producto } from '../producto';
+import { ProductoService } from '../producto.service';
 
 @Component({
   selector: 'app-producto-lista',
   templateUrl: './producto-lista.component.html',
-  styleUrls: ['./producto-lista.component.scss']
+  styleUrls: ['./producto-lista.component.scss'],
 })
 export class ProductoListaComponent {
+  productos: Producto[];
 
+  constructor(private productoServicio: ProductoService) {}
+
+  ngOnInit() {
+    this.obtenerProductos();
+  }
+
+  private obtenerProductos() {}
 }
